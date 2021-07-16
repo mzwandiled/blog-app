7c009b797709d3e38b5f406b0af61493a0689ea5
Mzwandile Lucky Dladla <mzwandiledladla7@gmail.com>
2021-07-16 14:49:29 +0200

    added custom style file

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,5 +1,21 @@
-<h2>Our Blog posts</h2>
+@extends('layouts.app')
 
-@foreach($blogs as $blog)
-    <p>{{$blog->title}}</p>
-@endforeach
+@section('content')
+    <main class="container">
+        <div class="container-fluid">
+            <div class="jumbotron ">
+                <h1>Our Blog posts</h1>
+            </div>
+            <div class="col-sm-12">
+                @foreach($blogs as $blog)
+                    <article>
+                        <h2>{{$blog->title}}</h2>
+                        <p>{{$blog->body}}</p>
+                    </article>
+                @endforeach
+            </div>
+        </div>
+    </main>
+
+<hr>
+@endsection
